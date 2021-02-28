d65ef477c905adf1dc5ff8b1c801c67d5032fa24
faishalnahidha <faishalnahidha@gmail.com>
2021-02-28 20:02:23 +0700

    Fix CMS dialog component

--- imports/ui/pages/Login.jsx
+++ imports/ui/pages/Login.jsx
@@ -138,7 +138,6 @@ export default function Login() {
               variant="outlined"
               autoComplete="current-password"
               fullWidth
-              required
               className={classes.inputForm}
               error={isPasswordError}
             />
